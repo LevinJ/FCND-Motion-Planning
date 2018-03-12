7b49dfea5a3b354372b659f88394da102c8996a4
Levin Jian <jianzhirong@gmail.com>
2018-03-12 08:02:25 +0800

    grid to graph exercise

--- exercise/astart_city.py
+++ exercise/astart_city.py
@@ -27,36 +27,88 @@ grid = create_grid(data, drone_altitude, safe_distance)
 
 # equivalent to
 # plt.imshow(np.flip(grid, 0))
-plt.imshow(grid, origin='lower') 
-
-plt.xlabel('EAST')
-plt.ylabel('NORTH')
-plt.show()
+# plt.imshow(grid, origin='lower') 
+# 
+# plt.xlabel('EAST')
+# plt.ylabel('NORTH')
+# plt.show()
 
 start_ne = (25,  100)
 goal_ne = (750., 370.)
-
+ 
 def heuristic_func(position, goal_position):
     h = math.fabs(position[0] - goal_position[0]) + math.fabs(position[1] - goal_position[1])
     return h
-
-start, goal = start_ne, goal_ne
-grid_flip = np.flip(grid, 0)
-path, cost = (grid_flip, heuristic_func, start, goal)
+ 
+path, cost = a_star(grid, heuristic_func, start_ne, goal_ne)
 print(len(path), cost)
+ 
+# plt.imshow(grid, cmap='Greys', origin='lower')
+#  
+# # For the purposes of the visual the east coordinate lay along
+# # the x-axis and the north coordinates long the y-axis.
+# plt.plot(start_ne[1], start_ne[0], 'x')
+# plt.plot(goal_ne[1], goal_ne[0], 'x')
+#  
+# pp = np.array(path)
+# plt.plot(pp[:, 1], pp[:, 0], 'g')
+#  
+# plt.xlabel('EAST')
+# plt.ylabel('NORTH')
+# plt.show()
+
+
+def point(p):
+    return np.array([p[0], p[1], 1.]).reshape(1, -1)
+
+def collinearity_check(p1, p2, p3, epsilon=1e-6):   
+    m = np.concatenate((p1, p2, p3), 0)
+    det = np.linalg.det(m)
+    return abs(det) < epsilon
+
+
+
+# We're using collinearity here, but you could use Bresenham as well!
+def prune_path(path):
+    pruned_path = [p for p in path]
+    # TODO: prune the path!
+    
+    i = 0
+    while i < len(pruned_path) - 2:
+        p1 = point(pruned_path[i])
+        p2 = point(pruned_path[i+1])
+        p3 = point(pruned_path[i+2])
+        
+        # If the 3 points are in a line remove
+        # the 2nd point.
+        # The 3rd point now becomes and 2nd point
+        # and the check is redone with a new third point
+        # on the next iteration.
+        if collinearity_check(p1, p2, p3):
+            # Something subtle here but we can mutate
+            # `pruned_path` freely because the length
+            # of the list is check on every iteration.
+            pruned_path.remove(pruned_path[i+1])
+        else:
+            i += 1
+    return pruned_path
+
+pruned_path = prune_path(path)
+print(len(pruned_path))
+
 
 plt.imshow(grid, cmap='Greys', origin='lower')
 
-# For the purposes of the visual the east coordinate lay along
-# the x-axis and the north coordinates long the y-axis.
 plt.plot(start_ne[1], start_ne[0], 'x')
 plt.plot(goal_ne[1], goal_ne[0], 'x')
 
-pp = np.array(path)
+pp = np.array(pruned_path)
 plt.plot(pp[:, 1], pp[:, 0], 'g')
+plt.scatter(pp[:, 1], pp[:, 0])
 
 plt.xlabel('EAST')
 plt.ylabel('NORTH')
+
 plt.show()
 
 
